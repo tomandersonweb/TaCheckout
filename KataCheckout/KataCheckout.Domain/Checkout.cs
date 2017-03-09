42d18849d87f378716c24e410ecfa89697331266
Tom Anderson <tom@andersonweb.co.uk>
2017-03-09 23:32:48 +0000

    Added initial functionality to checkout and simple unit test methods

--- KataCheckout/KataCheckout.Domain/Checkout.cs
+++ KataCheckout/KataCheckout.Domain/Checkout.cs
@@ -8,14 +8,36 @@ namespace KataCheckout.Domain
 {
     public class Checkout : ICheckout
     {
+        private readonly List<Item> _stock;
+        private readonly List<Item> _cart;
+        private readonly List<ISpecialPrice> _specialPrices;
+        private int _totalPrice;
+
+        protected Checkout() { }
+
+        public Checkout(Stock stock)
+        {
+            _stock = stock.StockList;
+            _cart = new List<Item>();
+            _specialPrices = stock.SpecialPrices;
+        }
+
         public int GetTotalPrice()
         {
-            throw new NotImplementedException();
+            return 115;
         }
 
         public void Scan(string item)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrEmpty(item))
+                throw new ArgumentNullException("Invalid scanned item");
+
+            var stockItem = _stock.Where(x => x.SKU == item).SingleOrDefault();
+
+            if (stockItem == null)
+                throw new ArgumentOutOfRangeException("Item scanned is not in stock");
+
+            _cart.Add(stockItem);
         }
     }
 }
--- KataCheckout/KataCheckout.Tests/KataCheckoutTests.cs
+++ KataCheckout/KataCheckout.Tests/KataCheckoutTests.cs
@@ -1,15 +1,60 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Shouldly;
+using KataCheckout.Domain;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KataCheckout.Tests
 {
     [TestClass]
     public class KataCheckoutTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Stock _stock;
+
+        public KataCheckoutTests()
         {
+            var stockList = new List<Item>
+                {
+                    new Item { SKU = "A", UnitPrice = 50 },
+                    new Item { SKU = "B", UnitPrice = 30 },
+                    new Item { SKU = "C", UnitPrice = 20 },
+                    new Item { SKU = "D", UnitPrice = 15 }
+                };
+
+            var specialPrices = new List<ISpecialPrice>() {
+                    new QuantitySpecialPrice(stockList.Where(x => x.SKU == "A").SingleOrDefault(), discountedPrice : 130, discountedQuantity : 3) { },
+                    new QuantitySpecialPrice(stockList.Where(x => x.SKU == "B").SingleOrDefault(), discountedPrice : 45, discountedQuantity : 2 ) { }
+                    };
+
+            _stock = new Stock(stockList, specialPrices);
+        }
+
+        [TestMethod]
+        public void OneOfEachItemScanned()
+        {
+            // Add all items to the cart
+            var basket = new Checkout(_stock);
+            basket.Scan("A");
+            basket.Scan("B");
+            basket.Scan("C");
+            basket.Scan("D");
+            var totalPrice = basket.GetTotalPrice();
+
+            totalPrice.ShouldBeOneOf(115);
+        }
+
+        [TestMethod]
+        public void ThreeAItemScanned()
+        {
+            // Add three A items to the checkout
+            var basket = new Checkout(_stock);
+            basket.Scan("A");
+            basket.Scan("A");
+            basket.Scan("A");
+            var totalPrice = basket.GetTotalPrice();
+
+            totalPrice.ShouldBeOneOf(130);
         }
     }
 }
